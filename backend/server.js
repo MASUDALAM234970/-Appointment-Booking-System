ccca88123d7f6b05bb7099d21a2abf3accd1df98
mdmas <mdmasudalam2349@gmail.com>
2025-02-10 13:11:30 +0600

    Add Cloudinary configuration and integrate it into the server

--- backend/server.js
+++ backend/server.js
@@ -2,25 +2,24 @@ import express from "express";
 import cors from "cors";
 import "dotenv/config";
 import { dbConnection } from "./config/mongodb.js";
+import connectCloudinay from "./config/cloudinary.js";
 
 // app config
-
 const app = express();
 const port = process.env.PORT || 5000;
+// Connect to MongoDB
+dbConnection();
+connectCloudinay();
 
 //middleware
-
 app.use(express.json());
 app.use(cors());
 
 //  api endPoint
-
 app.get("/", (req, res) => {
   res.send("Hello World");
 });
 
 app.listen(port, () => {
-  console.log(`server  is running on port ${port}`);
+  console.log(` Server is running on port ${port}`);
 });
-
-dbConnection();
