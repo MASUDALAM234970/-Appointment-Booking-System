90b2520cd89cc122cd9ef170f5313e26fe596396
mdmas <mdmasudalam2349@gmail.com>
2025-02-16 22:06:27 +0600

    Implement login form with state toggle for Admin and Doctor; update Tailwind CSS configuration for primary color

--- admin/src/pages/Login.jsx
+++ admin/src/pages/Login.jsx
@@ -1,5 +1,56 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function Login() {
-  return <div>Login</div>;
+  const [state, setState] = useState("Admin");
+
+  return (
+    <form className="min-h-[80vh] flex items-center">
+      <div className="flexa float-col gap-3 m-auto items-start p-8 min-w-[340px] sm:min-w-96 border rounded-xl text-[#5E5E5E] text-sm shadow-lg ">
+        <p className="text-2xl font-semibold m-auto ">
+          <span className="text-primary space-x-2">{state}</span>
+          Login
+        </p>
+        <div className="w-full">
+          <p>Email</p>
+          <input
+            className="border border-[#DADADA] rounded w-full p-2 mt-1"
+            type="email"
+            required
+          />
+        </div>
+        <div className="w-full">
+          <p>Password</p>
+          <input
+            className="border border-[#DADADA] rounded w-full p-2 mt-1"
+            type="password"
+            required
+          />
+        </div>
+        <button className="bg-primary text-white py-3 rounded w-full px-2 mt-1">
+          Login
+        </button>
+        {state === "Admin" ? (
+          <p>
+            Doctor Login ?
+            <span
+              className="text-primary space-x-2 cursor-pointer"
+              onClick={() => setState("Doctor")}
+            >
+              CLick here
+            </span>
+          </p>
+        ) : (
+          <p>
+            Admin Login ?
+            <span
+              className="text-primary space-x-2 cursor-pointer"
+              onClick={() => setState("Admin")}
+            >
+              CLick here
+            </span>
+          </p>
+        )}
+      </div>
+    </form>
+  );
 }
