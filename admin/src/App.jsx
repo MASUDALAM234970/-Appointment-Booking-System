ec53a5c802986d454cfb44891b313a667cb04b32
mdmas <mdmasudalam2349@gmail.com>
2025-02-17 14:36:51 +0600

    Preparation all Admin route page

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -7,6 +7,12 @@ import "./App.css";
 import Login from "./pages/Login";
 import { AdminContext } from "./context/AdminContext";
 import Navbar from "./Components/Navbar";
+import Sidebar from "./Components/Sidebar";
+import { Route, Routes } from "react-router-dom";
+import Dashboard from "./pages/Admin/Dashboard";
+import AddDoctor from "./pages/Admin/AddDoctor";
+import DoctorsList from "./pages/Admin/DoctorsList";
+import Allpointment from "./pages/Admin/Allpointment";
 
 function App() {
   const { atoken } = useContext(AdminContext);
@@ -14,6 +20,16 @@ function App() {
     <div className="bg-[#f8f9fd]">
       <ToastContainer />
       <Navbar />
+      <div className="flex items-start">
+        <Sidebar />
+        <Routes>
+          <Route path="/" element={<></>} />
+          <Route path="/admin-dashboard" element={<Dashboard />} />
+          <Route path="/add-doctor" element={<AddDoctor />} />
+          <Route path="/doctor-list" element={<DoctorsList />} />
+          <Route path="/all-appointments" element={<Allpointment />} />
+        </Routes>
+      </div>
     </div>
   ) : (
     <>
